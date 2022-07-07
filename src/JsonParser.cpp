827a6d93015e9f6c5c5327209863f9f4cbe2eb10
karlronsaria <andrew.mikedaniels@gmail.com>
2022-07-07 14:29:59 -0700

    routine commit

--- src/JsonParser.cpp
+++ src/JsonParser.cpp
@@ -1,6 +1,6 @@
 #include "JsonParser.h"
 
-int Json::Parser::Escape(Lexer & lexer) {
+int Json::Parsing::Escape(Lexer & lexer) {
     if (!lexer.NextChar())
         return Token::ERROR;
 
@@ -28,7 +28,7 @@ int Json::Parser::Escape(Lexer & lexer) {
     }
 }
 
-int Json::Parser::NextToken(Lexer & lexer) {
+int Json::Parsing::NextToken(Lexer & lexer) {
     if (!lexer.IgnoreWhiteSpace())
         return Token::END;
 
@@ -53,168 +53,4 @@ int Json::Parser::NextToken(Lexer & lexer) {
     int temp = (int)currentChar;
     lexer.NextChar();
     return temp;
-}
-
-int Json::Parser::NextToken() {
-    return _token = Json::Parser::NextToken(*_lexer);
-}
-
-Json::tree_t Json::Parser::GetTree() {
-    NextToken();
-
-    if (_token != '{')
-        return Error();
-
-    return ParseObject();
-}
-
-Json::tree_t Json::Parser::ParseObject() {
-    // TODO: Consider adding a kill condition at the
-    //   start of each Parse function.
-    std::vector<std::string> keys;
-    std::vector<Json::tree_t> values;
-    std::string key;
-    Json::tree_t value;
-
-    do {
-        ParsePair(key, value);
-
-        if (_token == Token::ERROR)
-            return Error();
-
-        keys.push_back(std::move(key));
-        values.push_back(std::move(value));
-        NextToken();
-    }
-    while (_token == ',');
-
-    if (_token != '}')
-        return Error();
-
-    return std::move(
-        std::make_unique<Object>(
-            std::move(keys),
-            std::move(values)
-        )
-    );
-}
-
-Json::tree_t Json::Parser::ParseList() {
-    auto values = std::vector<Json::tree_t>();
-
-    do {
-        auto value = ParseValue();
-
-        if (_token == Token::ERROR)
-            return Error();
-
-        values.push_back(std::move(value));
-        NextToken();
-    }
-    while (_token == ',');
-
-    if (_token != ']')
-        return Error();
-
-    return std::move(std::make_unique<List>(std::move(values)));
-}
-
-void Json::Parser::ParsePair(
-    std::string & key,
-    Json::tree_t & value
-) {
-    NextToken();
-
-    if ((Token)_token != Token::STRING) {
-        value = Error();
-        return;
-    }
-
-    key = _lexer->String();
-    NextToken();
-
-    if ((char)_token != ':') {
-        value = Error();
-        return;
-    }
-
-    value = std::move(ParseValue());
-}
-
-Json::tree_t Json::Parser::ParseValue() {
-    NextToken();
-
-    if (_token == '[')
-        return ParseList();
-
-    if (_token == '{')
-        return ParseObject();
-
-    return ParsePrimitive();
-}
-
-Json::tree_t Json::Parser::ParsePrimitive() {
-    switch ((Token)_token) {
-        case Token::WORD:
-            return ParseKeyword();
-        case Token::STRING:
-            return std::move(std::make_unique<String>(_lexer->String()));
-        default:
-            if ((char)_token == '-') {
-                NextToken();
-                return ParseNumber(true);
-            }
-    }
-
-    return ParseNumber(false);
-}
-
-Json::tree_t Json::Parser::ParseNumber(bool negative) {
-    int factor = negative ? -1 : 1;
-
-    switch ((Token)_token) {
-        case Token::INTEGER:
-            return std::move(std::make_unique<Numeric>(
-                Homonumeric::Integer(factor * _lexer->Integer())
-            ));
-        case Token::FLOAT:
-            return std::move(std::make_unique<Numeric>(
-                Homonumeric::Float(factor * _lexer->Float())
-            ));
-        default:
-            break;
-    }
-
-    return Error();
-}
-
-Json::tree_t Json::Parser::ParseKeyword() {
-    std::string keyword = _lexer->String();
-
-    if (keyword == "null")
-        return nullptr;
-
-    int value = 0;
-
-    if (keyword == "false")
-        value = 1;
-
-    if (keyword == "true")
-        value = 2;
-
-    if (!value)
-        return Error();
-
-    return std::move(std::make_unique<Numeric>(
-        Homonumeric::Boolean((bool)(value - 1))
-    ));
-}
-
-Json::tree_t Json::Parser::Error() {
-    // TODO: Log error
-    return nullptr;
-}
-
-Json::tree_t Json::Parser::Tree(Lexer * lexer) {
-    return Parser(lexer).GetTree();
 }
--- src/JsonParser.h
+++ src/JsonParser.h
@@ -6,32 +6,270 @@
 #include "JsonTree.h"
 
 namespace Json {
-    class Parser {
+    template <typename TreeT>
+    class ITreeFactory {
+        public:
+            typedef std::unique_ptr<TreeT>
+            ptr_t;
+
+            virtual ITreeFactory() = default;
+
+            virtual ptr_t NewObject(
+                std::vector<std::string> keys,
+                std::vector<ptr_t> values
+            ) = 0;
+
+            virtual ptr_t NewList(
+                std::vector<ptr_t> values
+            ) = 0;
+
+            virtual ptr_t NewString(
+                const std::string &
+            ) = 0;
+
+            virtual ptr_t Numeric(
+                Homonumeric
+            ) = 0;
+    };
+
+    class Parsing {
         public:
             static int Escape(Lexer &);
             static int NextToken(Lexer &);
+    };
+
+    template <class TreeT, template <class> class FactoryT>
+    class Parser {
+        public:
+            typedef ITreeFactory<TreeT>::ptr_t ptr_t;
         private:
+            std::unique_ptr<ITreeFactory<TreeT>>
+            _factory;
+
             Lexer * _lexer;
             int _token;
         protected:
             int NextToken();
-            tree_t ParseObject();
-            tree_t ParseList();
-            void ParsePair(std::string &, tree_t &);
-            tree_t ParseValue();
-            tree_t ParsePrimitive();
-            tree_t ParseNumber(bool);
-            tree_t ParseKeyword();
-            tree_t Error();
+            ptr_t ParseObject();
+            ptr_t ParseList();
+            void ParsePair(std::string &, ptr_t &);
+            ptr_t ParseValue();
+            ptr_t ParsePrimitive();
+            ptr_t ParseNumber(bool);
+            ptr_t ParseKeyword();
+            ptr_t Error();
         public:
             Parser(
                 Lexer * lexer
-            ):  _lexer(lexer),
+            ):  _factory(std::make_unique<FactoryT<TreeT>>),
+                _lexer(lexer),
                 _token(0) {}
 
-            tree_t GetTree();
-            static tree_t Tree(Lexer * lexer);
+            ptr_t GetTree();
+            static ptr_t Tree(Lexer * lexer);
     };
 };
+
+template <class T, template <class> class F>
+int
+Json::Parser<T, F>::NextToken() {
+    return _token = Json::Parsing::NextToken(*_lexer);
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::GetTree() {
+    NextToken();
+
+    if (_token != '{')
+        return Error();
+
+    return ParseObject();
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::ParseObject() {
+    // TODO: Consider adding a kill condition at the
+    //   start of each Parse function.
+    std::vector<std::string> keys;
+    std::vector<tree_t> values;
+    std::string key;
+    tree_t value;
+
+    do {
+        ParsePair(key, value);
+
+        if (_token == Token::ERROR)
+            return Error();
+
+        keys.push_back(std::move(key));
+        values.push_back(std::move(value));
+        NextToken();
+    }
+    while (_token == ',');
+
+    if (_token != '}')
+        return Error();
+
+    return std::move(
+        // TODO: TREE POINTER 1
+        _factory->NewObject(
+            std::move(keys),
+            std::move(values)
+        )
+    );
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::ParseList() {
+    auto values = std::vector<tree_t>();
+
+    do {
+        auto value = ParseValue();
+
+        if (_token == Token::ERROR)
+            return Error();
+
+        values.push_back(std::move(value));
+        NextToken();
+    }
+    while (_token == ',');
+
+    if (_token != ']')
+        return Error();
+
+    // TODO: TREE POINTER 2
+    return std::move(
+        _factory->NewList(std::move(values));
+    );
+}
+
+template <class T, template <class> class F>
+void
+Json::Parser<T, F>::ParsePair(
+    std::string & key,
+    Json::Parser<T, F>::ptr_t & value
+) {
+    NextToken();
+
+    if ((Token)_token != Token::STRING) {
+        value = Error();
+        return;
+    }
+
+    key = _lexer->String();
+    NextToken();
+
+    if ((char)_token != ':') {
+        value = Error();
+        return;
+    }
+
+    value = std::move(ParseValue());
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::ParseValue() {
+    NextToken();
+
+    if (_token == '[')
+        return ParseList();
+
+    if (_token == '{')
+        return ParseObject();
+
+    return ParsePrimitive();
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::ParsePrimitive() {
+    switch ((Token)_token) {
+        case Token::WORD:
+            return ParseKeyword();
+        case Token::STRING:
+            // TODO: TREE POINTER 3
+            return std::move(
+                _factory->NewString(_lexer->String())
+            );
+        default:
+            if ((char)_token == '-') {
+                NextToken();
+                return ParseNumber(true);
+            }
+    }
+
+    return ParseNumber(false);
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::ParseNumber(bool negative) {
+    int factor = negative ? -1 : 1;
+
+    switch ((Token)_token) {
+        case Token::INTEGER:
+            // TODO: TREE POINTER 4
+            return std::move(
+                _factory->NewNumeric(
+                    Homonumeric::Integer(factor * _lexer->Integer())
+                )
+            );
+        case Token::FLOAT:
+            // TODO: TREE POINTER 5
+            return std::move(
+                _factory->NewNumeric(
+                    Homonumeric::Float(factor * _lexer->Float())
+                )
+            );
+        default:
+            break;
+    }
+
+    return Error();
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::ParseKeyword() {
+    std::string keyword = _lexer->String();
+
+    if (keyword == "null")
+        return nullptr;
+
+    int value = 0;
+
+    if (keyword == "false")
+        value = 1;
+
+    if (keyword == "true")
+        value = 2;
+
+    if (!value)
+        return Error();
+
+    // TODO: TREE POINTER 6
+    return std::move(
+        _factory->NewNumeric(
+            Homonumeric::Boolean((bool)(value - 1))
+        )
+    );
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::Error() {
+    // TODO: Log error
+    return nullptr;
+}
+
+template <class T, template <class> class F>
+Json::Parser<T, F>::ptr_t
+Json::Parser<T, F>::Tree(Lexer * lexer) {
+    return Parser(lexer).GetTree();
+}
 
 #endif
